1383c417bb22a8079076c6c48b90cbdc2d368e9b
jcc98 <67444635+jcc98@users.noreply.github.com>
2021-12-01 22:41:09 +0100

    added select levels

---
+++ components/KanjiGameOptions.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const KanjiGameOptions = () => {
+    return (
+        <div className="overflow-hidden h-screen  p-5  flex justify-center content-center flex-col flex-wrap ">
+            <div>
+                <label >Select level</label>
+                <select name="select-level" id="select-level" className="w-full rounded py-2">
+                    <option value="5">５級</option>
+                    <option value="4">４級</option>
+                    <option value="3">３級</option>
+                    <option value="jun2">準２級</option>
+                    <option value="2">２級</option>
+                    <option value="jun1">準１一級</option>
+                    <option value="1">一級</option>
+                    <option value="all">５級から一級まで</option>
+                </select>
+            </div>
+            <div>
+                <label>Select number of hidden moji</label>
+                <select name="character-hide" id="character-hide" className="w-full rounded py-2">
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                </select>
+            </div>
+            <div>
+                <button className="bg-white py-4 rounded shadow-md mt-3 w-full hover:bg-gray-100">Start</button>
+            </div>
+        </div>
+    )
+}
+
+export default KanjiGameOptions
--- pages/kanji-game.tsx
+++ pages/kanji-game.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import KanjiGameOptions from '../components/KanjiGameOptions'
 import Nav from '../components/Nav'
+import {yojiAll} from "../public/json/yojiAll"
 
 const KanjiGame = () => {
+    // const yojiFive = yojiAll.filter((item) => item.grade === "5")
     return (
         <div>
             <Nav/>
+            <KanjiGameOptions/>
+
         </div>
     )
 }
